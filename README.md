cfe06135108bcd00c7722c770f106fd5de8782e6
Poomon <poomon2542@gmail.com>
2020-06-22 07:51:03 -0700

    first uppload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Vikehacks-2020
 Hackathon 2020
+
+I have used PHP, JS, CSS and HTML to create: 
+1. An interface of the website 
+2. A dynamic table on a homepage
+3. A form for manually adding quizzes
+4. A quiz page that allow users to attempt the quiz.
+
+In this competition, my partnert have built an auto generated quiz mode:
+1. It accepts a txt file.
+2. It scans and analzes the txt file to generate a wh-questions according to the uploaded text content.
+3. It generates the answers for each questions.
--- checkAnswer.php
+++ checkAnswer.php
@@ -7,6 +7,7 @@
 	$A3 = $_POST["A3"]; 
 	$A4 = $_POST["A4"]; 
 	$A5 = $_POST["A5"];
+	$error = false;
 
 	//call Json
 	$file = "quiz.json";
@@ -22,24 +23,24 @@
 	$phparray = json_decode($jsonstring, true);
 	for($i = 0; $i < sizeof($phparray); $i++){
 		if($phparray[$i]['ID'] == $ID){
-			if($phparray[$i]['answer_1'] != $A1){
-				$ErrA1 =  "'" . $A1 . "'" . " is wrong! the correct answer is " .  $phparray[$i]['answer_1'];
+			if(trim(strtolower($phparray[$i]['answer_1'])) != trim(strtolower($A1))){
+				$ErrA1 =  "'" . $A1 . "'" . " is wrong! the correct answer is " .  "'" . $phparray[$i]['answer_1'] . "'";
 			}
 
-			if($phparray[$i]['answer_2'] != $A2){
-				$ErrA2 = "'" . $A2 . "'" . " is wrong! the correct answer is " .  $phparray[$i]['answer_2'];
+			if(trim(strtolower($phparray[$i]['answer_2'])) != trim(strtolower($A2))){
+				$ErrA2 = "'" . $A2 . "'" . " is wrong! the correct answer is " .  "'" .$phparray[$i]['answer_2'] . "'";
 			}
 
-			if($phparray[$i]['answer_3'] != $A3){
-				$ErrA3 = "'" . $A3 . "'" . " is wrong! the correct answer is " .  $phparray[$i]['answer_3'];
+			if(trim(strtolower($phparray[$i]['answer_3'])) != trim(strtolower($A3))){
+				$ErrA3 = "'" . $A3 . "'" . " is wrong! the correct answer is " .  "'" .$phparray[$i]['answer_3'] . "'";
 			}
 
-			if($phparray[$i]['answer_4'] != $A4){
-				$ErrA4 = "'" . $A4 . "'" . " is wrong! the correct answer is " .  $phparray[$i]['answer_4'];
+			if(trim(strtolower($phparray[$i]['answer_4'])) != trim(strtolower($A4))){
+				$ErrA4 = "'" . $A4 . "'" . " is wrong! the correct answer is " .  "'" .$phparray[$i]['answer_4'] . "'";
 			}
 
-			if($phparray[$i]['answer_5'] != $A5){
-				$ErrA5 = "'" . $A5 . "'" . " is wrong! the correct answer is " .  $phparray[$i]['answer_5'];
+			if(trim(strtolower($phparray[$i]['answer_5'])) != trim(strtolower($A5))){
+				$ErrA5 = "'" . $A5 . "'" . " is wrong! the correct answer is " .  "'" . $phparray[$i]['answer_5'] . "'";
 			}
 
 			$Q1 = $phparray[$i]['question_1'];
@@ -51,7 +52,13 @@
 
 	}//for
 
+	echo "You completed the quiz.";
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+
 	if($ErrA1 != ""){
+		$error = true;
 		echo("1.)  " . $Q1);
 		echo "<br>";
 		echo($ErrA1);
@@ -59,6 +66,7 @@
 		echo "<br>";
 	}
 	if($ErrA2 != ""){
+		$error = true;
 		echo("2.)  " . $Q2);
 		echo "<br>";
 		echo($ErrA2);
@@ -66,6 +74,7 @@
 		echo "<br>";
 	}
 	if($ErrA3 != ""){
+		$error = true;
 		echo("3.)  " . $Q3);
 		echo "<br>";
 		echo($ErrA3);
@@ -73,6 +82,7 @@
 		echo "<br>";
 	}
 	if($ErrA4 != ""){
+		$error = true;
 		echo("4.)  " . $Q4);
 		echo "<br>";
 		echo($ErrA4);
@@ -80,6 +90,7 @@
 		echo "<br>";
 	}
 	if($ErrA5 != ""){
+		$error = true;
 		echo("5.)  " . $Q5);
 		echo "<br>";
 		echo($ErrA5);
@@ -87,5 +98,8 @@
 		echo "<br>";
 	}
 
+	if($error == false){
+		echo("All answers are correct.");
+	}
 
  ?>
